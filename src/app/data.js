9005adf5ea2f6706ac2e116363a7a86c583001b8
RaeveTheBrave <sultanrafi2806@gmail.com>
2025-01-01 22:44:41 +0700

    hehe manuk

--- src/app/data.js
+++ src/app/data.js
@@ -1,7 +1,7 @@
 import styles from "./page.module.css";
 
 import { app } from "./firebase";
-import { ref, getDatabase, get } from "firebase/database";
+import { ref, getDatabase, get, } from "firebase/database";
 
 export default async function Data() {
 
@@ -10,13 +10,15 @@ export default async function Data() {
 
     const data = (await get(reference)).val();
 
-
+    console.log(data);  
+    
     return (
         <div>
-            <h1>Current Time⏳: {data.CurrentTime}</h1>
-            <h1>Food Status🍌: {data.FoodStatus}</h1>
-            <h1>Pet Status😺: {data.PetStatus}</h1>
-            <h1>Water Status💧: {data.WaterStatus}</h1>
+            <h1>Current Time⏳: {data["Current Time"]}</h1>
+            <h1>Food Status🍌: {data["Food Status"]}</h1>
+            <h1>Pet Status😺: {data["Pet Status"]}</h1>
+            <h1>Water Status💧: {data["Water Status"]}</h1>
+            <h1>Specified Time: {data["Specified Time"]}</h1>
         </div>
     );
 }
