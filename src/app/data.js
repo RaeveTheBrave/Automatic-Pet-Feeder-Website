6fde4cfb152c5d8bf749070c4c9087f6c7221a96
RaeveTheBrave <sultanrafi2806@gmail.com>
2025-01-06 13:42:59 +0700

    updated UI with emotion.sh

--- src/app/data.js
+++ src/app/data.js
@@ -3,10 +3,10 @@
 import { getDatabase, ref, update } from "firebase/database";
 import { app } from "./firebase";
 import { useState } from "react";
-
+import { css } from '@emotion/css'
 // import styles from "./page.module.css";
 
-export default function Data({data}) {
+export default function Data({ data }) {
 
     const db = getDatabase(app);
     const reference = ref(db);
@@ -18,21 +18,49 @@ export default function Data({data}) {
     };
 
     return (
-        <div>
-            <h1>Current Time⏳: {data?.["Current Time"]}</h1>
-            <h1>Food Status🍌: {data?.["Food Status"]}</h1>
-            <h1>Pet Status😺: {data?.["Pet Status"]}</h1>
-            <h1>Water Status💧: {data?.["Water Status"]}</h1>
-            <h1>Specified Time: {data?.["Specified Time"]}</h1>
-            <br></br>
-            <input 
-            type="text" 
-            value={inputValue} 
-            onChange={(e) => setInputValue(e.target.value)}
-            placeholder="Input Specified Time"
-            >
-            </input>
-            <button onClick={handleInputChange}>Update</button>
-        </div>
+        <>
+            <div className={css`
+                    padding: 12px;
+                `}>
+                <h1>Current Time⏳: {data?.["Current Time"]}</h1>
+                <h1>Food Status🍌: {data?.["Food Status"]}</h1>
+                <h1>Pet Status😺: {data?.["Pet Status"]}</h1>
+                <h1>Water Status💧: {data?.["Water Status"]}</h1>
+                <h1>Specified Time🕛: {data?.["Specified Time"]}</h1>
+            </div>
+            <div className={css`
+                    display: flex;
+                    flex-flow: column; 
+                    `}>
+                <h2>Update Feeding Schedule</h2>
+                <br></br>
+                <input
+                    className={css`
+                        border-radius: 8px;
+                        border-color: white;
+                        font-size: 1rem;
+                        padding: 12px;
+                        `}
+                    type="text"
+                    value={inputValue}
+                    onChange={(e) => setInputValue(e.target.value)}
+                    placeholder="Input Specified Time"
+                >
+                </input>
+                <br></br>
+                <button
+                    className={css`
+                    border-radius: 8px;
+                    border-color: white;
+                    font-size: 1rem;
+                    padding: 12px;
+                    background-color:#121212;
+                    `}
+                    onClick={handleInputChange}
+                >
+                    Update
+                </button>
+            </div>
+        </>
     );
 }
